64c50b7ae6ec240d5f76224f1442c4cb7a616785
Theodor Sjöstedt <theodor.sjostedt@netlight.com>
2016-02-14 21:08:48 +0100

    finished third example

---
+++ 03-listen-closely.js
@@ -0,0 +1,36 @@
+// To run:
+// npm install && node 01-helloworld.js
+
+var Bot = require('slackbots');
+var CHANNEL = "#edgeacademy";
+
+var settings = {
+  token:  'TOKEN', //Replace this with your token
+  name: 'AcademyBot', //Replace this with the Bot's name
+  id: 'academybot' //Replace this with the Bot's handle
+};
+
+var params = {
+  icon_emoji: ':unicorn_face:', //replace this with whatever icon you want the bot to post as
+  link_names: 1 //Ensures formatting in messages posted by your bot is respected
+};
+
+var bot = new Bot(settings);
+
+bot.on('start', function() {
+  console.log("Started");
+});
+
+bot.on('message', function(data) {
+  if(data.type === "message") {
+    var text = data.text;
+    var user = bot._getUserById(data.user);
+    console.log("Got a message from " + user + ": " + text);
+  }
+});
+
+Bot.prototype._getUserById = function (userId) {
+  return this.users.filter(function (item) {
+    return item.id === userId;
+  })[0].name;
+};
