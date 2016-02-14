8b6fa7c6819f15245f1f3b90e8003c2709f5c596
Theodor Sjöstedt <theodor.sjostedt@netlight.com>
2016-02-14 20:53:30 +0100

    finished first example

---
+++ 01-helloworld.js
@@ -0,0 +1,23 @@
+// To run:
+// npm install && node 01-helloworld.js
+
+var Bot = require('slackbots');
+var CHANNEL = "#edgeacademy";
+
+var settings = {
+  token:  'TOKEN', //Replace this with your token
+  name: 'AcademyBot', //Replace this with the Bot's name
+  id: 'academybot' //Replace this with the Bot's handle
+};
+
+var params = {
+  icon_emoji: ':unicorn_face:', //replace this with whatever icon you want the bot to post as
+  link_names: 1 //Ensures formatting in messages posted by your bot is respected
+};
+
+var bot = new Bot(settings);
+
+bot.on('start', function() {
+  console.log("Started");
+  bot.postMessage(CHANNEL, "Hello from " + settings.name, params);
+});
